dff286d747627c9f8cc5bdea8e583c73118bce5f
Oriwei <lohyuenwei69@gmail.com>
2022-05-24 21:47:55 +0800

    adjust directory

---
+++ Gym_Management_System/src/main/java/Class/ManagerLoginClass.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Class;
+
+import java.io.File;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ManagerLoginClass {
+    private String userid;
+    private String password;
+
+    public ManagerLoginClass() {
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
